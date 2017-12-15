b901927dbd285586c7f542d447e5aa19573cfcf8
rosColunga <rosicolunga@hotmail.com>
2017-12-15 13:00:43 -0600

    Subiendo estructura básica y Tabs

--- js/app.js
+++ js/app.js
@@ -2,5 +2,14 @@
  * Funcionalidad de tu producto
  */
 
+ function openMe(inside) {
+   var i, content;
+   content = document.getElementsByClassName('content');
+   for (i = 0; i<content.length; i++) {
+     content[i].style.display= "none";
+   }
+   document.getElementById(inside).style.display= "block";
+ }
+
 // Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+//console.log(data);
